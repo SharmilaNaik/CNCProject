fd7b848e42fc2f7bbc137e93660e97f9b39a4444
Sharmila Naik <sharmilamadane717@gmail.com>
2022-12-30 22:39:20 +0530

    Sharmila Add new File Sales Add

--- src/test/java/com/ramee/CNC/Configuration.java
+++ src/test/java/com/ramee/CNC/Configuration.java
@@ -1,5 +1,7 @@
 package com.ramee.CNC;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -14,6 +16,9 @@ public class Configuration {
 	public void lounchBrowser() {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		FluentWait<WebDriver> wait=new FluentWait<WebDriver>(driver);
+		wait.withTimeout(Duration.ofSeconds(60));
+		wait.pollingEvery(Duration.ofSeconds(1));
 	}
 
 	@AfterMethod
--- src/test/java/com/ramee/CNC/Sales_Rep_AddSales.java
+++ src/test/java/com/ramee/CNC/Sales_Rep_AddSales.java
@@ -1,6 +1,7 @@
 package com.ramee.CNC;
 
 import java.time.Duration;
+import java.util.Set;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -8,8 +9,8 @@ import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.annotations.Test;
 
 public class Sales_Rep_AddSales extends Configuration {
-	@Test
-	public void loginSales_Rep() throws InterruptedException {
+	@Test(priority=1)
+	public void addSale_UsingloginSales_Rep() throws InterruptedException {
 		
 
 		driver.get("https://qa-cnc.rameesystems.com/");
@@ -49,8 +50,8 @@ public class Sales_Rep_AddSales extends Configuration {
 	    
 		
 	}
-	@Test
-	public void sales_Rep_AddCart_Clear() throws InterruptedException {
+	@Test(priority=2)
+	public void addCart_Clear_Sales_Rep_() throws InterruptedException {
 		
 		driver.get("https://qa-cnc.rameesystems.com/");
 		FluentWait wait = new FluentWait(driver);
@@ -91,7 +92,7 @@ public class Sales_Rep_AddSales extends Configuration {
 	    alert.accept();
 	    
 	}
-	@Test
+	@Test(priority=3)
 	public void viewSalesDetails() throws InterruptedException {
 		driver.get("https://qa-cnc.rameesystems.com/");
 		FluentWait wait = new FluentWait(driver);
@@ -119,4 +120,33 @@ public class Sales_Rep_AddSales extends Configuration {
 	    System.out.println(altMsg);
 	    alert.accept();
 	}
-}
+	
+	@Test(priority=4)
+	
+	public void view_SalesDetails_UsingOrderID() throws InterruptedException {
+		
+		driver.get("https://qa-cnc.rameesystems.com/");
+		FluentWait wait = new FluentWait(driver);
+		driver.findElement(By.xpath("//button[@class='btn dropdown-toggle']")).click();
+		driver.findElement(By.xpath("//input[@data-fv-field=\"identity\"]")).sendKeys("sales_rep");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("12345678");
+		wait.pollingEvery(Duration.ofMillis(500));
+		wait.withTimeout(Duration.ofSeconds(60));
+		driver.findElement(By.xpath("//button[@class='btn btn-block btn-success']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//div[@id=\"s2id_slcustomer\"]")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//input[@id=\"s2id_autogen29_search\"]")).sendKeys("ros");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div[@id=\"select2-drop\"]")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[@href='https://qa-cnc.rameesystems.com/admin/sales/customer_histry/118']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[@href=\"https://qa-cnc.rameesystems.com/admin/sales/view/216\"]")).click();
+		Thread.sleep(3000);
+		//driver.findElement(By.xpath("//button[@class=\"btn btn-default btn-default no-print pull-right\"]")).click();
+		//Thread.sleep(3000);
+		
+		 }
+	}
+
